a16d6ef13a040570768db54f9621258830de165f
Mahnoor <Mahnoorsaeed756@gmail.com>
2026-08-16 22:57:43 +0500

    Array

--- Java_script/practise/functions.js
+++ Java_script/practise/functions.js
@@ -9,9 +9,25 @@ const character = {
     health: 100
 };
 
-const calculateDamage = (heal, damage) => {
-    return heal - damage ;
+const calculateDamage = (health, damage) => {
+    return health - damage;
 };
 
 character.health = calculateDamage(character.health, 30);
 console.log(character.health);
+
+const getAliveCharacters = (char) => {
+    return char.filter( element => element.alive);
+};
+
+const alivechar = getAliveCharacters(characters)
+console.log(alivechar);
+
+//----------------------------------------------------------
+const calculateDamage = (health, damage) => {
+    health - damage; // no return 
+};
+
+const result = calculateDamage(100, 30);
+
+console.log(result);
